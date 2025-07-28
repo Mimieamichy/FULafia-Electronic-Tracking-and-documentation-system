d629b19d8a8cebe52267fd60489b38d86ead02c6
Amazingmercy <amaefulemercy499@gmail.com>
2025-07-28 23:12:53 +0200

    Major fix on modules

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -10,8 +10,8 @@ const router = Router();
 
 router.post('/upload', authenticate, checkPermission(Permission.UPLOAD_PROJECT), upload.single('project'), ProjectController.uploadProject);
 router.post('/uploadCorrected', authenticate, checkPermission(Permission.UPLOAD_PROJECT), upload.single('project'), ProjectController.supervisorUploadCorrection);
-router.get('/comments', authenticate, checkPermission(Permission.VIEW_COMMENTS), ProjectController.viewComments);
-router.get('/download/:studentId', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT), ProjectController.downloadProject);
-router.post('/comment', authenticate, checkPermission(Permission.COMMENT), ProjectController.commentOnProject);
+router.get('/comments/:studentId/:versionNumber', authenticate, checkPermission(Permission.VIEW_COMMENTS), ProjectController.viewComments);
+router.get('/download/:studentId/:versionNumber', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT), ProjectController.downloadProject);
+router.post('/comment/:studentId/:versionNumber', authenticate, checkPermission(Permission.COMMENT), ProjectController.commentOnProject);
 
 export default router;
