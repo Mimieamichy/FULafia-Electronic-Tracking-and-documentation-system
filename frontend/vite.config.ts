145e0df1ea127f5c01d709bb2675aeabc30fcf54
Amazingmercy <amaefulemercy499@gmail.com>
2025-07-29 15:48:11 +0200

    Minor fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
