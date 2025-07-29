eec87ef7a5c5329875e8529b1eff3952f62d889e
Amazingmercy <amaefulemercy499@gmail.com>
2025-07-29 16:11:36 +0200

    Minor fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,13 +6,17 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   server: {
+    port: 8080, // Default Vite port for local development
+    host: "::",
+    strictPort: true, // Don't try other ports if this is taken
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL,
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000', // Fallback for local dev
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    },
+    }
   },
   resolve: {
     alias: {
