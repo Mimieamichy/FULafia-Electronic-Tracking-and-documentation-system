c1ec8427269c53fdb9a3cb5c53b6e51be7fb4624
Amazingmercy <amaefulemercy499@gmail.com>
2025-09-27 02:52:02 +0200

    fixes

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -12,7 +12,7 @@ router.get('/:userId', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT
 router.post('/upload', authenticate, checkPermission(Permission.UPLOAD_PROJECT), upload.single('project'), ProjectController.uploadProject);
 router.post('/uploadCorrected', authenticate, checkPermission(Permission.UPLOAD_PROJECT), upload.single('project'), ProjectController.supervisorUploadCorrection);
 router.get('/comments/:studentId/:versionNumber', authenticate, checkPermission(Permission.VIEW_COMMENTS), ProjectController.viewComments);
-router.get('/download/:studentId/:versionNumber', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT), ProjectController.downloadProject);
+router.get('/download/:studentId/', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT), ProjectController.downloadProject);
 router.get('/download/:studentId/', authenticate, checkPermission(Permission.DOWNLOAD_PROJECT), ProjectController.downloadLatestProject);
 router.post('/comment/:studentId/:versionNumber', authenticate, checkPermission(Permission.COMMENT), ProjectController.commentOnProject);
 router.post('/approve/:studentId', authenticate, checkPermission(Permission.APPROVE_STUDENT_PROJECT), ProjectController.approveProject);
